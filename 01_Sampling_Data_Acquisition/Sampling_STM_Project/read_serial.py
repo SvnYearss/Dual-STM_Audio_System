2f2d712210ba7f9c1fb79549f1fd9b5198d8bdfb
SvnYearss <ruotai02@gmail.com>
2026-04-15 21:49:15 +0800

    >Upload Week 6 Lab Code

---
+++ 01_Sampling_Data_Acquisition/Sampling_STM_Project/read_serial.py
@@ -0,0 +1,9 @@
+import serial
+ser = serial.Serial(port = "/dev/tty.usbmodem11303", baudrate = 230400, bytesize = 8, parity = "N", stopbits = 1, timeout = 5)
+print(ser.name)
+file_1 = open("raw_ADC_values.data", "wb")
+for i in range(100):
+    x = ser.read(500)
+    file_1.write(x)
+file_1.close()
+ser.close
